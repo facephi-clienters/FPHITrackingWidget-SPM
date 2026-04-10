cce17483a891148e5797ba39f94e3c9c4990e669
Jose Antonio <joseantoniojuan@facephi.com>
2026-04-10 12:12:19 +0200

    New version: 1.3.0

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FPHITrackingWidget",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "FPHITrackingWidget",
+            targets: ["FPHITrackingWidget-SPM"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        //.package(url: "git@github.com:fingerprintjs/fingerprintjs-pro-ios.git", .upToNextMinor(from: "2.8.0")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "FPHITrackingWidget-SPM",
+            dependencies: [
+                "FPHITrackingWidget",
+                //.product(name: "FingerprintPro", package: "fingerprintjs-pro-ios")
+            ]),
+        .binaryTarget(
+            name: "FPHITrackingWidget",
+            url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/WIDGET/FPHITrackingWidget/1.3.0/FPHITrackingWidget.zip",
+            checksum: "19c60170f42ab3aee3f112e4b59491889cf1f2e3a2fc213cbb7b35344dacecf5"
+        )
+    ]
+)
---
+++ README.md
@@ -0,0 +1,1 @@
+# FPHITrackingWidget-SPM
