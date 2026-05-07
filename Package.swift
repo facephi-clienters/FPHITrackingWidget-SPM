64a9227237a09fb0a45a9001c2ee77b254b7570a
Facephi <developer@facephi.com>
2026-05-07 11:05:36 +0000

    Promote FPHITrackingWidget SPM 1.4.3

--- Package.swift
+++ Package.swift
@@ -1,33 +1,37 @@
-// swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
-    name: "FPHITrackingWidget",
+    name: "FPHITrackingWidget-SPM",
+    platforms: [
+        .iOS(.v13),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FPHITrackingWidget",
-            targets: ["FPHITrackingWidget-SPM"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        //.package(url: "git@github.com:fingerprintjs/fingerprintjs-pro-ios.git", .upToNextMinor(from: "2.8.0")),
+            targets: [
+                "FPHITrackingWidget",
+            ]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FPHITrackingWidget-SPM",
-            dependencies: [
-                "FPHITrackingWidget",
-                //.product(name: "FingerprintPro", package: "fingerprintjs-pro-ios")
-            ]),
-        .binaryTarget(
             name: "FPHITrackingWidget",
-            url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/WIDGET/FPHITrackingWidget/1.4.0/FPHITrackingWidget.zip",
-            checksum: "91922b62dcd56a3cf207d6da7e47d4ef3432efc7e871862ed21286f6d1bc799b"
-        )
+            dependencies: [
+                "FPHITrackingCore",
+                "_LocationEssentials",
+            ],
+            path: "Sources/FPHITrackingWidget"
+        ),
+        .binaryTarget(
+            name: "FPHITrackingCore",
+            url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/WIDGET/FPHITrackingWidget/1.4.3/FPHITrackingCore.zip",
+            checksum: "ffcf693d95236fce2240440a8045bb8f8d364ffe9668385a1e6f7a143244326f"
+        ),
+        .binaryTarget(
+            name: "_LocationEssentials",
+            url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/WIDGET/FPHITrackingWidget/1.4.3/_LocationEssentials.zip",
+            checksum: "df06d01bb107b1b3e65cd1a97a70f600ea6f4a2fad85469cefb5727d852d763c"
+        ),
     ]
 )
